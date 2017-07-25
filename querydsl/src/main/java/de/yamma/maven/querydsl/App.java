53d6d588153b99bb2267681f5c747d81a44759d0
Alexander Schwartz <erSchistan@gmail.com>
2017-07-25 20:49:27 +0200

    Migrating to MySQL.
    Generation of sources works in GitBash with mvn generate-sources

--- querydsl/src/main/java/de/yamma/maven/querydsl/App.java
+++ querydsl/src/main/java/de/yamma/maven/querydsl/App.java
@@ -1,21 +1,6 @@
 package de.yamma.maven.querydsl;
 
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.bson.Document;
-import org.mongodb.morphia.Datastore;
-import org.mongodb.morphia.Morphia;
-
-import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
-import com.mongodb.client.MongoDatabase;
-import com.querydsl.mongodb.morphia.MorphiaQuery;
-
-import de.yamma.maven.querydsl.entities.QSport;
-import de.yamma.maven.querydsl.entities.Sport;
 
 /**
  * Hello world!
@@ -25,58 +10,6 @@ public class App {
 	public static void main(String[] args) throws UnknownHostException {
 		System.out.println("Hello World!");
 
-		QSport sport = QSport.sport;
-
-		String username = "admin";
-		String password = "";
-		String host = "";
-		int port = 27017;
-		String dbName = "admin";
-
-				
 		
-		// Manage the mongo db connection...
-		List<ServerAddress> seeds = new ArrayList<>();
-		seeds.add( new ServerAddress(host, port ));
-		List<MongoCredential> credentials = new ArrayList<>();
-		credentials.add(
-		    MongoCredential.createCredential(
-		        username,
-		        dbName,
-		        password.toCharArray()
-		    )
-		);
-		MongoClient mongoClient = new MongoClient( seeds, credentials );
-		
-//		MongoClient mongoClient = new MongoClient(new MongoClientURI(
-//				host != null ? "mongodb://" + username + ":" + password + "@" + host + ':' + port : "mongodb://localhost:27017"));
-
-		MongoDatabase db = mongoClient.getDatabase(dbName);
-
-		Morphia morphia = new Morphia();
-		// https://stackoverflow.com/questions/21859579/authentication-during-connection-to-mongodb-server-instance-using-java
-
-		while (true) {
-			try {
-			
-				Datastore datastore = morphia.createDatastore(mongoClient, db.getName());
-				MorphiaQuery<Sport> query = new MorphiaQuery<>(morphia, datastore, sport);
-				Sport mysport = query.where(sport.name.eq("Laufen")).fetchOne();
-				
-				if (mysport == null)
-				{
-					mysport = new Sport();
-					mysport.setName("Laufen");
-					
-					  Document document = new Document();
-				        document.put("person", "Test");
-			        db.getCollection("user").insertOne(document);  
-				}
-			} catch (Throwable e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				continue;
-			}
-		}
 	}
 }
--- querydsl/src/main/java/de/yamma/maven/querydsl/entities/QSport.java
+++ querydsl/src/main/java/de/yamma/maven/querydsl/entities/QSport.java
@@ -8,37 +8,62 @@ import com.querydsl.core.types.PathMetadata;
 import javax.annotation.Generated;
 import com.querydsl.core.types.Path;
 
+import com.querydsl.sql.ColumnMetadata;
+import java.sql.Types;
+
+
+
 
 /**
- * QSport is a Querydsl query type for Sport
+ * QSport is a Querydsl query type for QSport
  */
-@Generated("com.querydsl.codegen.EntitySerializer")
-public class QSport extends EntityPathBase<Sport> {
+@Generated("com.querydsl.sql.codegen.MetaDataSerializer")
+public class QSport extends com.querydsl.sql.RelationalPathBase<QSport> {
 
-    private static final long serialVersionUID = -1165814981L;
+    private static final long serialVersionUID = -1864539492;
 
     public static final QSport sport = new QSport("sport");
 
     public final BooleanPath hasDistance = createBoolean("hasDistance");
 
-    public final NumberPath<Long> id = createNumber("id", Long.class);
+    public final NumberPath<Integer> id = createNumber("id", Integer.class);
 
-    public final NumberPath<Long> lastUpdate = createNumber("lastUpdate", Long.class);
+    public final DatePath<java.sql.Date> lastUpdate = createDate("lastUpdate", java.sql.Date.class);
 
     public final StringPath name = createString("name");
 
-    public final BooleanPath uploaded = createBoolean("uploaded");
+    public final com.querydsl.sql.PrimaryKey<QSport> primary = createPrimaryKey(id);
 
     public QSport(String variable) {
-        super(Sport.class, forVariable(variable));
+        super(QSport.class, forVariable(variable), "null", "sport");
+        addMetadata();
     }
 
-    public QSport(Path<? extends Sport> path) {
-        super(path.getType(), path.getMetadata());
+    public QSport(String variable, String schema, String table) {
+        super(QSport.class, forVariable(variable), schema, table);
+        addMetadata();
+    }
+
+    public QSport(String variable, String schema) {
+        super(QSport.class, forVariable(variable), schema, "sport");
+        addMetadata();
+    }
+
+    public QSport(Path<? extends QSport> path) {
+        super(path.getType(), path.getMetadata(), "null", "sport");
+        addMetadata();
     }
 
     public QSport(PathMetadata metadata) {
-        super(Sport.class, metadata);
+        super(QSport.class, metadata, "null", "sport");
+        addMetadata();
+    }
+
+    public void addMetadata() {
+        addMetadata(hasDistance, ColumnMetadata.named("HAS_DISTANCE").withIndex(3).ofType(Types.BIT));
+        addMetadata(id, ColumnMetadata.named("ID").withIndex(1).ofType(Types.INTEGER).withSize(10).notNull());
+        addMetadata(lastUpdate, ColumnMetadata.named("LAST_UPDATE").withIndex(4).ofType(Types.DATE).withSize(10));
+        addMetadata(name, ColumnMetadata.named("NAME").withIndex(2).ofType(Types.LONGVARCHAR).withSize(65535).notNull());
     }
 
 }
